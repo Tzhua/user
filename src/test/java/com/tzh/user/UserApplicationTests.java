49c2cf005722991654c1cbf4e62cb3669baee8e3
汤振华 <13164377125@163.com>
2023-12-07 14:19:27 +0800

    修改druid的版本

--- src/test/java/com/tzh/user/UserApplicationTests.java
+++ src/test/java/com/tzh/user/UserApplicationTests.java
@@ -1,13 +1,14 @@
 package com.tzh.user;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
-class UserApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes =UserApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserApplicationTests {
+
 
 }
